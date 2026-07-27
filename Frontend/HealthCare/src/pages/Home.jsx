baefcc0e7b27b91fb79a684bf75172b0215b6ae8
abusufiyan7518 <abusufiyantechsak@example.com>
2026-07-27 18:59:37 +0530

    feat: add testimonials preview to home page

--- Frontend/HealthCare/src/pages/Home.jsx
+++ Frontend/HealthCare/src/pages/Home.jsx
@@ -1,11 +1,17 @@
 import Hero from "../components/home/Hero.jsx";
 import AboutPreview from "../components/home/AboutPreview.jsx";
-
+import TestimonialsSection from "../components/testimonials/TestimonialsSection";
 function Home() {
   return (
     <main>
       <Hero />
       <AboutPreview />
+      <TestimonialsSection
+  limit={3}
+  showViewAllButton={true}
+  title="What Our Patients Say"
+  subtitle="Real experiences shared by patients who trusted us with their healthcare."
+/>
     </main>
   );
 }
